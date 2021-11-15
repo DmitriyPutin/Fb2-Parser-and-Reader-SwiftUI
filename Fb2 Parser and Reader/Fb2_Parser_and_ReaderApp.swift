178eff330aed27d959ad81a68be1e18e9659f51d
Putin Dmitriy <putin.1970@gmail.com>
2021-11-16 09:30:14 +1100

    Initial Commit

---
+++ Fb2 Parser and Reader/Fb2_Parser_and_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fb2_Parser_and_ReaderApp.swift
+//  Fb2 Parser and Reader
+//
+//  Created by Dmitriy Putin on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Fb2_Parser_and_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
